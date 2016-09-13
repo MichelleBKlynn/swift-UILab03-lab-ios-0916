ce6e1eb2b08c8846593fa8561e2fab8a0985449c
MichelleBKlynn <michellestaton@gmail.com>
2016-09-13 13:02:41 -0400

    Complete answers

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -9,11 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var suitLabel: UILabel!
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBOutlet weak var lowerSuitLabel: UILabel!
+    
     // TODO: Create IB outlets
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
+        
+        
 
+    }
+    
+    
     // TODO: IB actions and code to update UI
+    
+    @IBAction func clover(sender: UIButton) {
+        suitLabel.text = "♣️"
+        lowerSuitLabel.text = "♣️"
+        valueLabel.text = "4"
+    }
+    
+    
+    @IBAction func spade(sender: UIButton) {
+        suitLabel.text = "♠️"
+        lowerSuitLabel.text = "♠️"
+        valueLabel.text = "3"
+    }
+    
+    
+    @IBAction func diamond(sender: UIButton) {
+        suitLabel.text = "♦️"
+        lowerSuitLabel.text = "♦️"
+        valueLabel.text = "8"
+        
+    }
+    
+    
+    @IBAction func heart(sender: UIButton) {
+        suitLabel.text = "♥️"
+        lowerSuitLabel.text = "♥️"
+        valueLabel.text = "10"
+   
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//    func suitLabelChanges(buttonPressed: UIButton) -> String {
+//        var buttonPressed = UIButton()
+//        
+//        switch true {
+//        case buttonPressed == UIButton(clover):
+//            return "♣️"
+//            
+//        case buttonPressed == UIButton(spade):
+//            return "♠️"
+//            
+//        case buttonPressed == UIButton(diamond):
+//            return "♦️"
+//            
+//        case buttonPressed == UIButton(heart):
+//            return "♥️"
+//            
+//        default:
+//            return "Press a button"
+//        }
+//        
+//        
+//    }
+    
+
